7a24296cd5ff0dd29281f919e552ec90a63ad3e9
tsina-tefe <tsinukaltefera34@gmail.com>
2026-03-08 11:39:08 +0300

    move weather API call to backend and hadle with express route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import React, { useEffect, useState } from "react"
-import Header from "./components/Header"
-import Input from "./components/Input"
-import OutputDisplay from "./components/OutputDisplay"
-const apiKey = import.meta.env.VITE_WEATHER_API_KEY
-
-console.log(apiKey);
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import OutputDisplay from "./components/OutputDisplay";
 
 function App() {
-
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
   const [input, setInput] = useState("");
@@ -15,58 +10,64 @@ function App() {
 
   useEffect(() => {
     async function getWeather() {
-      if(!city) return;
+      if (!city) return;
       try {
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
-        //if(!response.ok) throw new Error("Fetching error");
+        const response = await fetch("http://localhost:3000/api/weather", {
+          method: "POST",
+          headers: {
+            "content-Type": "application/json",
+          },
+          body: JSON.stringify({ city }),
+        });
+        if (!response.ok) {
+          const error = await response.json();
+          const errorMessage = error.message;
+          throw new Error(errorMessage);
+        }
         const data = await response.json();
-        if(data.error) throw new Error(`${data.error.message}`);
+
         setWeather(data);
-        console.log(data);
-      } catch(error) {
+      } catch (error) {
         setError(error.message);
-        console.log(error.message);
       }
     }
     getWeather();
-    
-  },[city]);
+  }, [city]);
 
   const handleSet = () => {
     setCity(input.trim());
     setInput("");
   };
 
-  const showError = () => {
-    // if(error) {
-    //   return error;
-    // }
-    setTimeout(() => {
-      setError("");
-    }, 1000);
-  }
   return (
     <>
       <Header />
       <div className="content">
-         <div className="location-section">
-            <label className="label-text">LOCATION</label>
-            <div className="input-wrapper">
-                <input type="text" placeholder='Enter City' onChange={(e) => {
-                  setInput(e.target.value);
-                }} />
-                <button className="set-btn" onClick={handleSet}>SET</button>
-            </div>
-            <p className="error">{error ? error : ""}{showError()}</p>
+        <div className="location-section">
+          <label className="label-text">LOCATION</label>
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="Enter City"
+              onChange={(e) => {
+                setInput(e.target.value);
+                setError("");
+              }}
+            />
+            <button className="set-btn" onClick={handleSet}>
+              SET
+            </button>
+          </div>
+          <p className="error">{error ? error : ""}</p>
         </div>
         <OutputDisplay weather={weather} />
       </div>
       <footer className="footer">
-          <span className="footer-text">MY WEATHER</span>
-          <div className="footer-line"></div>
+        <span className="footer-text">MY WEATHER</span>
+        <div className="footer-line"></div>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
